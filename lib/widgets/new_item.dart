cb0c94140e7f3c8b82bc3b8bc9307d223bf6d80a
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2024-06-21 15:03:34 -0300

    228. Avoiding Unnecessary Requests

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/category.dart';
 import 'package:http/http.dart' as http;
+import 'package:shopping_list_app/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -37,13 +38,19 @@ class _NewItemState extends State<NewItem> {
         }),
       );
 
-      print(response.body);
-      print(response.statusCode);
+      final Map<String, dynamic> resData = json.decode(response.body);
 
       if (!context.mounted) {
         return;
       }
-      Navigator.of(context).pop();
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: resData['name'],
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory!,
+        ),
+      );
     }
   }
 
