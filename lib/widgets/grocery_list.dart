a46d1915d3431d7f84327540c0724ae7a92cfd77
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2024-06-25 12:34:52 -0300

    231. Sending DELETE Requests

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -76,10 +76,25 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
+
+    final Uri url = Uri.https(
+      'mobile-test-85bed-default-rtdb.firebaseio.com',
+      'shopping-list/${item.id}.json',
+    );
+
+    final response = await http.delete(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
+
   }
 
   @override
